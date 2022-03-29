70f0c4f2955dd5f1011d50a944d20e69ce2ee31b
iqamac <hyun77320@naver.com>
2022-03-29 22:41:25 +0900

    2022.03.29.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'; //eslint-disable-line
+import React, { useState } from 'react'; //eslint-disable-line
 import Counter from './Counter'; //eslint-disable-line
 import Info from './Info'; //eslint-disable-line
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button 
+        onClick={() => {
+          setVisible(!visible);
+      }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
