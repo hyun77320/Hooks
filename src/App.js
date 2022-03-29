050380643ff6830f951c6dee2bf41ecf5b2685c7
iqamac <hyun77320@naver.com>
2022-03-29 19:25:53 +0900

    2022.03.29.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; //eslint-disable-line
+import Counter from './Counter'; //eslint-disable-line
+import Info from './Info'; //eslint-disable-line
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <Info />;
+};
 
 export default App;
